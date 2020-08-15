ec4aa619f0926cf2d793e16d9650f7a2d86af628
simranbirla <62006675+simranbirla@users.noreply.github.com>
2020-08-16 02:35:34 +0530

    difficulty onload

--- src/components/Difficulty.js
+++ src/components/Difficulty.js
@@ -1,30 +1,32 @@
 import React, { useState, useRef } from "react";
 
-const Difficulty = () => {
-  const local = () => {
-    if (localStorage.getItem("level")) {
-      console.log(select.current);
-      return localStorage.getItem("level");
-    } else {
-      localStorage.setItem("level", "easy");
-      console.log(select.current);
-      return localStorage.getItem("level");
-    }
-  };
+const local = () => {
+  if (localStorage.getItem("level")) {
+    return localStorage.getItem("level");
+  } else {
+    localStorage.setItem("level", "easy");
+    return localStorage.getItem("level");
+  }
+};
 
+const Difficulty = () => {
   const [level, setLevel] = useState(local());
+  const selectRef = useRef(local());
+
+  window.onload = () => {
+    selectRef.current.value = local();
+  };
 
   const changeLevel = (e) => {
     setLevel(e.target.value);
     localStorage.setItem("level", e.target.value);
   };
 
-  console.log(level);
   return (
     <div style={{ margin: "auto" }}>
       <label style={{ margin: "20px auto", textAlign: "center" }}>
         Choose a difficulty level:
-        <select onChange={(e) => changeLevel(e)}>
+        <select onChange={(e) => changeLevel(e)} ref={selectRef}>
           <option value="easy">Easy</option>
           <option value="medium">Medium</option>
           <option value="difficult">Difficult</option>
