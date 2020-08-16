74126b40d388456300fb5f81d4ddc83fc2832099
simranbirla <62006675+simranbirla@users.noreply.github.com>
2020-08-16 19:49:54 +0530

    time increase

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,24 +1,36 @@
 import React, { useRef } from "react";
-import Time from "./Time";
 import { useState } from "react";
+import { useEffect } from "react";
 
 const Input = (props) => {
   const inputRef = useRef("");
-
+  const [time, setTime] = useState(10);
   const changeWord = (e) => {
     if (e.target.value === props.random_word) {
       console.log(true);
       inputRef.current.value = "";
+      setTime(time + 2);
       props.setRandomWord();
       return true;
     }
   };
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTime(time - 1);
+    }, 1000);
+    return () => {
+      clearInterval(interval);
+    };
+  }, [time]);
+
   return (
     <>
       <label>
         Type the word here:
         <input type="text" onChange={(e) => changeWord(e)} ref={inputRef} />
       </label>
+      <p>Time left:{time}</p>
     </>
   );
 };
