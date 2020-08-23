b23e508cfaa52c83ad9f8251c8a20d68d6ea7261
simranbirla <62006675+simranbirla@users.noreply.github.com>
2020-08-23 20:10:33 +0530

    fetch API

--- src/components/Word.js
+++ src/components/Word.js
@@ -1,21 +1,37 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Input from "./Input";
 import "../Style/Word.css";
 
-const Word = (props) => {
-  var randomWords = require("random-words");
+const generateWord = async (word, setWord) => {
+  const resposne = await fetch(
+    "https://api.wordnik.com/v4/words.json/randomWord?hasDictionaryDef=true&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=5&maxLength=-1&api_key=1r4puztkfs6evf77y4iriehwmg2u0sxox2vr2xj8g847m1iep"
+  );
+  const data = await resposne.json();
+  setWord(data.word);
+  return word;
+};
 
-  const [random, setRandom] = useState(randomWords());
+const Word = (props) => {
+  //var randomWords = require("random-words");
+
+  //const [random, setRandom] = useState(randomWords());
+  const [words, setWords] = useState("");
+
+  useEffect(() => {
+    generateWord(words, setWords);
+
+    return;
+  }, []);
 
   return (
     <div className="word-container">
       <div className="heading">
-        <h1>The word is ::{random}</h1>
+        <h1>The word is ::{words}</h1>
       </div>
       <div className="input-container">
         <Input
-          random_word={random}
-          setRandomWord={() => setRandom(randomWords())}
+          random_word={words}
+          setRandomWord={() => generateWord(words, setWords)}
           level={props.level}
         />
       </div>
